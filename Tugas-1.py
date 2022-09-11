b99a289320b26e51201b33dc70ce59f4e8b4c999
ibnufjr <21083010106@student.upnjatim.ac.id>
2022-09-11 16:54:00 +0700

    Commit Tugas

---
+++ Tugas-1.py
@@ -0,0 +1,14 @@
+print("-"*35)
+print("TUGAS SISOP DAY 1 KELAS B")
+print("-"*35)
+print("Nama	: MOHAMAD IBNU FAJAR MAULANA")
+print("Npm	: 21083010106")
+print("-"*35)
+print("Notes:")
+print("-"*35)
+print("Halo Dunia")
+print("ini adalah skrip Bash pertamaku di Linux")
+
+
+
+
---
+++ Tugas-1.sh
@@ -0,0 +1,12 @@
+echo "-----------------------------------"
+echo "TUGAS SISOP DAY 1 KELAS B"
+echo "-----------------------------------"
+echo "Nama 	: Mohamad Ibnu Fajar Maulana"
+echo "Npm 	: 21083010106"
+echo "-----------------------------------"
+echo "Notes"
+echo "-----------------------------------"
+echo "halo dunia"
+echo "ini adalah skrip Bash pertamaku di Linux"
+
+
